78281052d1b52a68dec70c489c53eb68d8ccfd36
Oleg Shilov <shilov-1@yandex.ru>
2023-11-20 10:27:47 +0300

    preventing cache;

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const { readFile } = require('fs').promises;
+const { nanoid } = require('nanoid');
 
 const { host, port } = require('./env.js');
 
@@ -10,18 +11,22 @@ let params = null;
 
 const requestListener = async function (req, res) {
   // todo: пресеты типа psp, nokia и т.д.
-  // todo: генерация картинок без текста по параметру text=true например. отдавать index.js с другой html.
-  //  или менять в html путь к картинке ./bg.jpg -> bg[hash][timestamp].jpg ил типа того - для предотвращения кэша
+  // todo: генерация картинок без добавления текста по параметру text=true например
 
   if (req.url === '/' || req.url.includes('/?')) {
     params = url.parse(req.url, true).query; // именно в этом месте нужно получать параметры
 
     const contents = await readFile(__dirname + '/psp/index.html');
 
+    const contentsReplaced = contents.toString().replace(
+      'src="./bg.jpg"',
+      `src="./bg_${nanoid()}.jpg"`
+    );
+
     res.setHeader('Content-Type', 'text/html');
     res.writeHead(200);
-    res.end(contents);
-  } else if (req.url === '/bg.jpg') {
+    res.end(contentsReplaced);
+  } else if (req.url.includes('/bg')) {
     const contents = await getImageWithText(params);
 
     res.setHeader('Content-Type', 'image/jpeg');
